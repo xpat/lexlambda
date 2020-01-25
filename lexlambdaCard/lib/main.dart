eab5a5a76a1681bfdafe4b38ca4e2c7e08bc7aa1
Chef Tech <cheftech@pinchepoutine.digital>
2020-01-25 09:56:57 -0500

    add various pics to image folder

--- lexlambdaCard/lib/main.dart
+++ lexlambdaCard/lib/main.dart
@@ -15,21 +15,22 @@ class MyApp extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.brown,
-              ),
+
+             // Container(
+              //  width: 100.0,
+               // color: Colors.brown,
+              //),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   CircleAvatar(
                     radius: 150.0,
-                    backgroundImage: ExactAssetImage('images/profilepic2020.jpg'),
+                    backgroundImage: ExactAssetImage('images/SmokinGunORIG.webp'),
                   ),
                   Text(
                     'Christophe R. Patraldo',
                     style: TextStyle(
-                      fontFamily: 'Varela Round',
+                      fontFamily: 'Source Sans Pro',
                       fontSize: 40.0,
                       color: Colors.lightGreenAccent.shade700,
                       fontWeight: FontWeight.normal,
@@ -38,31 +39,44 @@ class MyApp extends StatelessWidget {
                   Expanded(
                     child: Container(
                       height: 100.0,
-                      width: 400.0,
+                      width: 200.0,
                       padding: EdgeInsets.all(20.0),
                       color: Colors.yellow,
-                      child: Text(
-                        'FLUTTER DEVELOPER',
-                        style: TextStyle(
-                          fontFamily: 'Varela Round',
-                          fontSize: 55.0,
-                          color: Colors.blueGrey.shade100,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.pink,
+                        size: 24.0,
+                        semanticLabel: 'Text to announce in acccessibility modes',
                       ),
-                    ),
+                      Icon(Icons.audiotrack, color: Colors.green, size: 30.0),
+                      Icon(Icons.beach_access, color: Colors.black, size: 36.0),
+                      ],
                   ),
+                  ),
+        ),
                   Container(
                     height: 100.0,
-                    width: 100.0,
+                    width: 400.0,
                     color: Colors.lightGreenAccent,
+                    child: Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'Varela Round',
+                      fontSize: 55.0,
+                      color: Colors.blueGrey.shade100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   ),
                 ],
               ),
-              Container(
-                width: 100.0,
-                color: Colors.brown,
-              ),
+              //Container(
+               // width: 100.0,
+                //color: Colors.brown,
+              //),
             ],
           ),
         ),
