b5872736c9ac9608295a39635090dc3622047ad0
Chef Tech <cheftech@pinchepoutine.digital>
2020-01-27 10:18:05 -0500

    ambientx flutter simple  business card

--- lexlambdaCard/lib/main.dart
+++ lexlambdaCard/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.lightBlueAccent.shade700,
-          body: SafeArea(
-            child: Column(children: <Widget>[
-              SizedBox(height: 20.0),
+        backgroundColor: Colors.black54,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 10.0),
               CircleAvatar(
                 radius: 100.0,
                 backgroundImage: AssetImage('images/profilepic2020.jpg'),
@@ -22,24 +25,69 @@ class MyApp extends StatelessWidget {
                 'Christophe R. Patraldo',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
+                  fontSize: 30.0,
+                  color: Colors.amber.shade500,
                   fontWeight: FontWeight.normal,
                 ),
               ),
               Text(
-                'LINUX ADMIN / DEVOPS (HYBRID-CLOUD) ENGINEER',
+                'Ambient Computing',
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
                   fontSize: 25.0,
-                  color: Colors.blueGrey.shade100,
+                  color: Colors.blue.shade100,
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                height: 20.0,
+                width: 150.0,
+                color: Colors.grey.shade600,
+                child: const Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              Card(
+                // default color is white, so no need to include color on next line
+                // color: Colors.grey.shade600,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.amber.shade600),
+                  title: Text(
+                    '(+1) 323 691-2121',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                // default color is white, so no need to include color on next line
+                //  color: Colors.grey.shade600,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.amber.shade600,
+                  ),
+                  title: Text(
+                    'cheftech@ambientx.com',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
             ],
+          ),
         ),
-        ),
-        ),
+      ),
     );
   }
 }
